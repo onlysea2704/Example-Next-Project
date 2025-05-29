50090828a64363cf73f4e551a6d83e71f676cdf9
onlysea2704 <phamduyhai2704@gmail.com>
2025-05-29 10:25:35 +0700

    m

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,5 +3,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  console.log({message: 'Success'})
   return NextResponse.json({ message: 'Hello from modern Next.js API!' });
 }
