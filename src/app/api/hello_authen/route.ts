88ef4dca8ea1173fea686be8187c8ba64bd540de
onlysea2704 <phamduyhai2704@gmail.com>
2025-06-02 09:44:10 +0700

    m

--- src/app/api/hello_authen/route.ts
+++ src/app/api/hello_authen/route.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server'
 
 export async function GET(req: NextRequest) {
     if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return new NextResponse(`Unauthorized ${process.env.CRON_SECRET}`, { status: 401 });
+        return new NextResponse(`Unauthorized`, { status: 401 });
     }
     console.log({ message: 'Success' })
     return NextResponse.json({ message: 'Hello from modern Next.js API!' });
